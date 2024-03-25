89184a33549dece4e65f599ff517f31032036fcd
MarkussMik <99797623+ThatEmoKidXD@users.noreply.github.com>
2024-03-25 11:26:42 +0200

    p5.js

---
+++ lecture-5/sketch.js
@@ -0,0 +1,37 @@
+let circleX= 100;
+let circleY= 100;
+
+
+
+function setup () {
+    createCanvas(windowWidth,windowHeight);
+    
+}
+
+function draw () {
+    background(0);
+
+    fill('white');
+    noStroke();
+    circle(mouseX, mouseY, 50);
+
+    stroke('pink');
+    noFill();
+    circle(circleX, circleY,50);
+
+    if (mouseX > circleX -25 && mouseX < circleX +25) {
+
+        if(mouseY > circleY -25 && mouseY < circleY +25){
+        //fill('pink')
+        //circle(width / 2, height /2, 100)
+
+        circleX= random(0, width);
+        circleY= random(0, height);
+        }
+    }
+
+    
+
+
+
+}
